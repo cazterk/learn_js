a48f27bc34e70376e4cf242d0e2ad969cd3a65e8
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2022-01-29 15:21:46 +0200

    arrays

--- learn.js
+++ learn.js
@@ -73,4 +73,90 @@ var ourStr = "first. ";
 ourStr += "second.";
 
 // constructing strings with variables
-console.log(firstName + lastName);
+console.log(firstName + lastName + " is the name");
+
+// appending variables to strings
+var str3 = "awesome";
+var ourStr3 = "God is ";
+ourStr3 += str3;
+
+// find lenght of string
+console.log("lenth:", str3.length);
+
+// bracket notation: find nth character in string
+console.log("index of letter :", str3[0]);
+
+// string immutability
+var myStr3 = "jello world";
+myStr3 = "hello word";
+
+// bracket notation: find last character in string even when lenght is unknown
+console.log("last letter of", str3, "is", `'${str3[str3.length - 1]}'`);
+
+// bracket notation: find nth-to-last character in string
+console.log("nth-to-last of", str3, "is", `'${str3[str3.length - 3]}'`);
+
+// word blanks
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  var result = "";
+  result +=
+    "the " +
+    myAdjective +
+    " " +
+    myNoun +
+    " " +
+    myVerb +
+    " to the store " +
+    myAdverb;
+  return result;
+}
+
+console.log(wordBlanks("dog", "big", "ran", "quickly"));
+console.log(wordBlanks("bike", "slow", "flew", "slowly"));
+
+// store multiple values with arrays
+var ourArray = ["caz", 24];
+
+// nested arrays
+var nestArr = [
+  ["one", 2],
+  ["two", 1],
+  ["three", 9],
+  ["four", 4],
+  [1, 2, 3, 5, 3, 23, 3],
+  13,
+  14,
+];
+
+// access array data with indexes
+var ourArray2 = [1, 2, 3, 4];
+
+// modify array data with indexes
+ourArray2[1] = 5;
+
+// access multi-demensional arrays with indexs
+var myData = nestArr[3][1];
+console.log(myData);
+
+// manipulate arrays with push()
+ourArray2.push(["abc", "hi"]);
+
+// manipulate arrays with pop()
+var removeFromOurArray = ourArray2.pop();
+
+// manipulate arrays with shift()
+var removeFromOurArray = ourArray2.shift();
+
+// manipulate arrays with unshift()
+ourArray2.unshift("way");
+console.log(ourArray2);
+
+// shopping list
+
+var myList = [
+  ["cereal", 3],
+  ["milk", 2],
+  ["yougurt", 2],
+  ["juice", 1],
+  ["bananas", 4],
+];
