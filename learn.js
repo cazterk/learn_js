6692ff96cd14e7b747af063041283a207a3748ad
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2022-01-29 11:15:13 +0200

    initial commit

---
+++ learn.js
@@ -0,0 +1,47 @@
+/* undefines, null, boolean, string, symbol, number, and object */
+
+var myName = "Caz";
+
+myName = 8;
+
+let ourName = "freeCodeCamp";
+
+const pi = 3.14;
+
+var a;
+var b = 10;
+var c = "string";
+
+a = 7;
+
+b = a;
+
+console.log(a); //run => ctrl + alt + n
+
+a = a + 1;
+b = b + 5;
+c = c + "string2";
+
+// declarations
+var studyCapVar;
+
+// adding
+var sum = 10 + 10;
+
+// subtraction
+var difference = 45 - 33;
+
+// multiplying
+var product = 5 * 5;
+
+// dividing
+var quotient = 66 / 33;
+
+// incrementing
+var myVar = 7;
+myVar++;
+
+// decrementing
+var myVar2 = 8;
+myVar2--;
+console.log(myVar2);
