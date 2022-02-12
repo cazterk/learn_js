17e68ea11ef704663876656ba0cbb2b3ab20edc7
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2022-02-12 14:33:06 +0200

    const keyword

--- learn.js
+++ learn.js
@@ -965,9 +965,51 @@ function checkScope() {
   let i = "function scope";
   if (true) {
     let i = "block scope";
-    console.log("block scope i is: ", i);
+    // console.log("block scope i is: ", i);
   }
-  console.log("function scope i is: ", i);
+  // console.log("function scope i is: ", i);
   return i;
 }
-console.log(checkScope());
+// console.log(checkScope());
+
+// read=only variable with const keyword
+function printManyTimes(str) {
+  "use strict";
+  const SENTENCE = str + "is cool";
+  // sentence = str + " is amazing!";
+
+  for (let i = 0; i < str.length; i += 2) {
+    console.log(SENTENCE);
+  }
+}
+// printManyTimes("caz");
+
+// mutate an array declared with const
+const s = [5, 7, 2];
+function editInPlace() {
+  "use strict";
+  // s = [2, 5, 7];
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+}
+editInPlace();
+// console.log(s);
+
+// prevent object mutation
+function freezeObj() {
+  "use strict";
+  const MATH_CONSTANTS = {
+    PI: 3.14,
+  };
+  Object.freeze(MATH_CONSTANTS);
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch (ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+
+const PI = freezeObj();
+// console.log(PI);
