bb4a88e6858c8197fed99fec77be118a4bcbde10
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2022-02-12 18:04:24 +0200

    imports

---
+++ test.js
@@ -0,0 +1,7 @@
+const capitalizeString = (str) => {
+  return str.CharAt(0).toUpperCase() + String.slice(1);
+};
+
+export { capitalizeString };
+export const foo = "bar";
+export const bar = "foo";
