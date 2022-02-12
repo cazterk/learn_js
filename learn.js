c4960b0187ed21f22451fcdbe5de47a4dbd31889
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2022-02-12 17:06:02 +0200

    template literals

--- learn.js
+++ learn.js
@@ -1139,4 +1139,39 @@ const half = (function () {
   };
 })();
 // console.log(stats);
-console.log(half(stats));
+// console.log(half(stats));
+
+// create strings using template leterals
+const person = {
+  name: "jotson",
+  age: 24,
+};
+
+const ageCheck = `hello mr ${person.name}, your age is ${person.age} right?`;
+// console.log(ageCheck);
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"],
+};
+function makeList(arr) {
+  const resultDisplayArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    resultDisplayArray.push(`<li class="text-warning">${arr[i]}</li>`);
+  }
+
+  return resultDisplayArray;
+}
+const resultDisplayArray = makeList(result.failure);
+// console.log(resultDisplayArray);
+
+// write concise object literal declaration using simple fields
+const createPerson = (name, age, gender) => ({
+  name,
+  age,
+  gender,
+});
+// console.log(createPerson("jotson tembo", 24, "male"));
+
+//  write concise declarative functions
