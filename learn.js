997654bfe239e8800c070169359ac4e36ca7c2b2
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2022-02-12 12:59:06 +0200

    random operations

--- learn.js
+++ learn.js
@@ -893,3 +893,19 @@ var data = lookupPrifle("caz", "hello");
 // console.log(data);
 
 // genrate random fractions
+function randomFraction() {
+  return Math.random();
+}
+
+// console.log(randomFraction());
+
+// genrate random whole numbers
+
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+  return Math.floor(Math.random() * 10);
+}
+// console.log(randomWholeNum());
+
+// generate random whole numbers within a range
