8670b285d79bf8a0c93bb41c6d39f8606bb2bda7
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2023-02-03 13:20:54 +0200

    add factional function js

--- misc/misc.js
+++ misc/misc.js
@@ -1,61 +1,68 @@
+let x = {
+  a: 1,
+  b: 2,
+};
 
-let x ={
-  a:1,
-  b:2
+let Arr = [];
+for (let i in x) {
+  Arr.push(i);
 }
+console.log(Arr);
+let c = "hi";
+let y = "ih";
 
-let Arr = []
-for (let i in x){
-  Arr.push(i)
-}
-console.log(Arr)
-let c = 'hi'
-let y = 'ih'
+const revStr = (str) => str.split("").reverse().join("");
+console.log(revStr(c) === y);
 
-const revStr=(str) => str.split('').reverse().join('')
-console.log((revStr(c) ===y))
-
-let x2={
-  a:1,
-  b:2,
-  getA(){
-    console.log(this.a)
-    return this
+let x2 = {
+  a: 1,
+  b: 2,
+  getA() {
+    console.log(this.a);
+    return this;
   },
-  getB(){
-    console.log(this.a)
-  }
+  getB() {
+    console.log(this.a);
+  },
+};
+
+console.log(x2.getA().getB());
+
+function foo() {
+  return 5;
 }
 
-console.log(x2.getA().getB())
+let myvar = foo;
 
+console.log(myvar);
 
-function foo(){
-  return 5
-}
-
-let myvar = foo
-
-console.log(myvar)
-
-let arr = [1,2,5,9,4]
+let arr = [1, 2, 5, 9, 4];
 
 // check sum indexes
-const checkSum = (num, arr) =>{
-  for(let i = 0; i< arr.length; i++){
-    for(let j = 0; j< arr.length; j++){
-      let sum =  arr[i] + arr[j]
-      if(sum === num) return [i, j]
+const checkSum = (num, arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      let sum = arr[i] + arr[j];
+      if (sum === num) return [i, j];
     }
   }
-}
+};
 
-console.log(checkSum(3, arr))
+console.log(checkSum(3, arr));
 
 // sum of array elements
-const sum = (...args) =>{
-let total = 0
- args.forEach((x)=>total+=x)
- return total
-}
-console.log(sum(1,2,3))
+const sum = (...args) => {
+  let total = 0;
+  args.forEach((x) => (total += x));
+  return total;
+};
+console.log(sum(1, 2, 3));
+
+const fact = (n) => (n ? n * fact(n - 1) : 1);
+const functioral = (...args) => {
+  const filterArr = args.filter((x) => x != 1 && x != 0);
+  let factArr = filterArr.map(fact);
+
+  return factArr;
+};
+console.log("fact :", functioral(0, 1, 2, 3, 4));
